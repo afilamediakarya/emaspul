2876e9c2570626c84249068a84c0c635aa7a13d5
andi akmalul yaqin <94356363+aakmalulyaqin@users.noreply.github.com>
2022-09-02 02:55:11 +0800

    Update dokumenDesaController.php

--- app/Http/Controllers/dokumenDesaController.php
+++ app/Http/Controllers/dokumenDesaController.php
@@ -508,8 +508,10 @@ class dokumenDesaController extends Controller
     } 
 
     public function data_alokasi_desa(){
-
+        
         $perangkat_desa = DB::table('user')->select('perangkat_desa.id_desa')->join('perangkat_desa','user.id_unit_kerja','perangkat_desa.id')->where('user.id',Auth::user()->id)->first();
+
+        
 
         $result = [];
        $data = DB::table('unit_kerja')->select('id','nama_unit_kerja')->whereRaw("id<>''")->get();
@@ -519,40 +521,23 @@ class dokumenDesaController extends Controller
         foreach($data as $unit_kerja_list){
             $unit_kerja_list->Dpa=DB::table('dpa')
             ->join('sub_kegiatan', 'dpa.id_sub_kegiatan', '=', 'sub_kegiatan.id')
-            ->join('pegawai_penanggung_jawab', 'dpa.id_pegawai_penanggung_jawab', '=', 'pegawai_penanggung_jawab.id')
-            ->select('dpa.id','dpa.nilai_pagu_dpa','sub_kegiatan.nama_sub_kegiatan','sub_kegiatan.kode_sub_kegiatan','pegawai_penanggung_jawab.nama_lengkap as ppk')
+            ->select('dpa.id','dpa.nilai_pagu_dpa','sub_kegiatan.nama_sub_kegiatan')
             ->where('dpa.tahun',$tahun)
             ->where('dpa.id_unit_kerja',$unit_kerja_list->id)
             ->get();
-
             foreach ( $unit_kerja_list->Dpa as $dpa ){
-                
-            $dpa->Paket1=DB::table('paket_dau')
-                ->selectRaw("paket_dau.id,paket_dau.satuan,paket_dau.id_sumber_dana_dpa,paket_dau.nama_paket,paket_dau.volume,paket_dau.pagu,paket_dau.keterangan,@jenis_paket:='dau' as jenis_paket,sumber_dana_dpa.sumber_dana")
-                ->join('sumber_dana_dpa','sumber_dana_dpa.id','=','paket_dau.id_sumber_dana_dpa')
-                ->whereRaw("paket_dau.id_dpa='$dpa->id'");
-                $dpa->Paket=DB::table('paket_dak')
+            $dpa->Paket=DB::table('paket_dak')
                 ->selectRaw("paket_dak.id,paket_dak.satuan,paket_dak.id_sumber_dana_dpa,paket_dak.nama_paket,paket_dak.volume,paket_dak.anggaran_dak as pagu,@keterangan:='' as keterangan,@jenis_paket:='dak' as jenis_paket,sumber_dana_dpa.sumber_dana")
                 ->join('sumber_dana_dpa','sumber_dana_dpa.id','=','paket_dak.id_sumber_dana_dpa')
                 ->whereRaw("paket_dak.id_dpa='$dpa->id' AND sumber_dana_dpa.jenis_belanja='Belanja Modal'")
-                ->union($dpa->Paket1)->get();
-
+                ->get();
                 foreach($dpa->Paket as $paket){
-                    if($paket->jenis_paket=='dau'){
-                        $paket->Lokasi=DB::table('paket_dau_lokasi')
-                        ->join('desa','paket_dau_lokasi.id_desa','=','desa.id')
-                        ->join('kecamatan','desa.id_kecamatan','=','kecamatan.id')
-                        ->select('desa.nama as nama_desa','kecamatan.nama as nama_kecamatan')
-                        ->whereRaw("id_paket_dau='$paket->id' AND desa.id=".$perangkat_desa->id_desa)
-                        ->get();
-                    }else{
                         $paket->Lokasi=DB::table('paket_dak_lokasi')
                         ->join('desa','paket_dak_lokasi.id_desa','=','desa.id')
                         ->join('kecamatan','desa.id_kecamatan','=','kecamatan.id')
                         ->select('desa.nama as nama_desa','kecamatan.nama as nama_kecamatan')
                         ->whereRaw("id_paket_dak='$paket->id' AND desa.id=".$perangkat_desa->id_desa)
                         ->get();
-                    }
                     $paket->Desa='';
                     $paket->Kecamatan='';
                     foreach($paket->Lokasi as $lokasi){
@@ -648,39 +633,33 @@ class dokumenDesaController extends Controller
         //Margin PDF
         $spreadsheet->getActiveSheet()->getPageMargins()->setTop(0.3);
         $spreadsheet->getActiveSheet()->getPageMargins()->setRight(0.3);
-        $spreadsheet->getActiveSheet()->getPageMargins()->setLeft(0.5);
+        $spreadsheet->getActiveSheet()->getPageMargins()->setLeft(0.8);
         $spreadsheet->getActiveSheet()->getPageMargins()->setBottom(0.3);
-        // if ($sumber_dana_selected == 'semua') {
-        //     $sumber_dana_selected = 'DAK FISIK & NON FISIK';
-        // } else {
-        //     $sumber_dana_selected = strtoupper($sumber_dana_selected);
-        // }
 
         $sheet->setCellValue('A1', 'LAPORAN DAFTAR ALOKASI PEMBANGUNAN');
-        $sheet->setCellValue('A2', 'tes');
-        $sheet->setCellValue('A3', 'PEMERINTAH KABUPATEN ENREKANG TAHUN ANGGARAN ' . $tahun );
-        $sheet->mergeCells('A1:I1');
-        $sheet->mergeCells('A2:I2');
-        $sheet->mergeCells('A3:I3');
+        //$sheet->setCellValue('A2', 'tes');
+        $sheet->setCellValue('A2', 'PEMERINTAH KABUPATEN ENREKANG TAHUN ANGGARAN ' . $tahun );
+        $sheet->mergeCells('A1:H1');
+        $sheet->mergeCells('A2:H2');
+        $sheet->mergeCells('A3:H3');
         $sheet->getStyle('A1')->getFont()->setSize(12);
 
-        $sheet->getStyle('A:I')->getAlignment()->setWrapText(true);
-        $sheet->getStyle('A1:I6')->getFont()->setBold(true);
+        $sheet->getStyle('A:H')->getAlignment()->setWrapText(true);
+        $sheet->getStyle('A1:H6')->getFont()->setBold(true);
 
         $sheet->setCellValue('A5', 'NO')->mergeCells('A5:A6');
-        $sheet->setCellValue('B5', 'URAIAN KEGIATAN')->mergeCells('B5:B6')->getColumnDimension('B')->setWidth(50);
-        $sheet->setCellValue('C5', 'PAGU')->mergeCells('C5:C6')->getColumnDimension('C')->setWidth(15);
+        $sheet->setCellValue('B5', 'URAIAN KEGIATAN')->mergeCells('B5:B6')->getColumnDimension('B')->setWidth(45);
+        $sheet->setCellValue('C5', 'PAGU')->mergeCells('C5:C6')->getColumnDimension('C')->setWidth(25);
         $sheet->setCellValue('D5', 'LOKASI')->mergeCells('D5:E5');
-        $sheet->setCellValue('F5', 'VOLUME')->mergeCells('F5:F6')->getColumnDimension('F')->setWidth(12);
-        $sheet->setCellValue('G5', 'PPK/PPTK')->mergeCells('G5:G6')->getColumnDimension('G')->setWidth(25);
-        $sheet->setCellValue('H5', 'SUMBER DANA')->mergeCells('H5:H6')->getColumnDimension('H')->setWidth(20);
-        $sheet->setCellValue('I5', 'KET')->mergeCells('I5:I6')->getColumnDimension('I')->setWidth(20);
+        $sheet->setCellValue('F5', 'SUMBER DANA')->mergeCells('F5:F6')->getColumnDimension('F')->setWidth(15);
+        $sheet->setCellValue('G5', 'VOLUME')->mergeCells('G5:G6')->getColumnDimension('G')->setWidth(15);
+        $sheet->setCellValue('H5', 'KET')->mergeCells('H5:H6')->getColumnDimension('H')->setWidth(20);
 
 
-        $sheet->setCellValue('D6', 'DESA/KEL')->mergeCells('D6:D6')->getColumnDimension('D')->setWidth(15);
-        $sheet->setCellValue('E6', 'KECAMATAN')->mergeCells('E6:E6')->getColumnDimension('E')->setWidth(15);
+        $sheet->setCellValue('D6', 'DESA/KEL')->mergeCells('D6:D6')->getColumnDimension('D')->setWidth(20);
+        $sheet->setCellValue('E6', 'KECAMATAN')->mergeCells('E6:E6')->getColumnDimension('E')->setWidth(20);
 
-        $sheet->getStyle('A5:I6')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setRGB('C6E0B4');
+        $sheet->getStyle('A5:H6')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setRGB('C6E0B4');
         $cell = 7;
         $tot_pagu=0;
 
@@ -689,11 +668,10 @@ class dokumenDesaController extends Controller
                 continue;
             }
             $sheet->getRowDimension($cell)->setRowHeight(25);
-            $sheet->getStyle('A'.$cell.':I'.$cell)->getFont()->setBold(true);
+            $sheet->getStyle('A'.$cell.':H'.$cell)->getFont()->setBold(true);
             $sheet->setCellValue('B'.$cell,$res->nama_unit_kerja)->mergeCells('B'.$cell.':B'.$cell);
-            $sheet->setCellValue('C'.$cell, 'Rp. '.number_format($res->Pagu))->mergeCells('C'.$cell.':C'.$cell);
-            $sheet->setCellValue('D'.$cell,'')->mergeCells('D'.$cell.':I'.$cell);
-            $sheet->getStyle('A' . $cell . ':I' . $cell)->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setRGB('CC99FF');
+            $sheet->setCellValue('C'.$cell, 'Rp. '.number_format($res->Pagu))->mergeCells('D'.$cell.':D'.$cell);
+            $sheet->getStyle('A' . $cell . ':H' . $cell)->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setRGB('CC99FF');
             $tot_pagu+=$res->Pagu;
             $cell++;
             $i = 0;
@@ -705,13 +683,7 @@ class dokumenDesaController extends Controller
                         $sheet->setCellValue('A' . $cell, ++$i);
                         $sheet->setCellValue('B' . $cell, $dpa->nama_sub_kegiatan);
                         $sheet->setCellValue('C' . $cell, 'Rp. '.number_format($dpa->Pagu));
-                        $sheet->setCellValue('D' . $cell, '');
-                        $sheet->setCellValue('E' . $cell, '');
-                        $sheet->setCellValue('F' . $cell, '');
-                        $sheet->setCellValue('G' . $cell, '');
-                        $sheet->setCellValue('H' . $cell, '');
-                        $sheet->setCellValue('I' . $cell, '');
-                        $sheet->getStyle('A' . $cell . ':I' . $cell)->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setRGB('D9E1F2');
+                        $sheet->getStyle('A' . $cell . ':H' . $cell)->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setRGB('D9E1F2');
                         $cell++;
                       }
                    }
@@ -726,16 +698,14 @@ class dokumenDesaController extends Controller
                                 $sheet->setCellValue('C' . $cell, 'Rp. '.number_format($paket->pagu));
                                 if (!empty($paket->Desa)) {
                                     $sheet->setCellValue('D' . $cell, $paket->Desa);
-                                    
                                     $sheet->setCellValue('E' . $cell, rtrim($paket->Kecamatan, ", "));
                                 }else{
                                     $sheet->setCellValue('D' . $cell, '');
                                     $sheet->setCellValue('E' . $cell, '');
-                                }   
+                                }
                                 $sheet->setCellValue('F' . $cell, $paket->volume.' '.$paket->satuan);
-                                $sheet->setCellValue('G' . $cell, $dpa->ppk);
-                                $sheet->setCellValue('H' . $cell, $paket->sumber_dana);
-                                $sheet->setCellValue('I' . $cell, $paket->keterangan);
+                                $sheet->setCellValue('G' . $cell, $paket->sumber_dana);
+                                $sheet->setCellValue('H' . $cell, $paket->keterangan);
                                 $cell++;
     
                     }
@@ -743,24 +713,22 @@ class dokumenDesaController extends Controller
         }
 
         $sheet->getRowDimension($cell)->setRowHeight(25);
-        $sheet->getStyle('A'.$cell.':I'.$cell)->getFont()->setBold(true);
+        $sheet->getStyle('A'.$cell.':H'.$cell)->getFont()->setBold(true);
         $sheet->setCellValue('B'.$cell,'TOTAL ')->mergeCells('B'.$cell.':B'.$cell);
-        $sheet->setCellValue('C'.$cell, 'Rp. '.number_format($tot_pagu))->mergeCells('C'.$cell.':C'.$cell);
-        $sheet->setCellValue('D'.$cell,'')->mergeCells('D'.$cell.':I'.$cell);
-        $sheet->getStyle('A'.$cell.':I'. $cell)->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setRGB('C6E0B4');
+        $sheet->setCellValue('C'.$cell, 'Rp. '.number_format($tot_pagu));
+        $sheet->getStyle('A'.$cell.':H'. $cell)->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setRGB('C6E0B4');
 
-        $sheet->getStyle('A1:I' . $cell)->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER);
-        $sheet->getStyle('A1:I' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
-        $sheet->getStyle('A4:A' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+        $sheet->getStyle('A1:H' . $cell)->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER);
+        $sheet->getStyle('A1:H' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+        //$sheet->getStyle('A4:A' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
         $sheet->getStyle('B7:B' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
         $sheet->getStyle('C7:C' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
-        $sheet->getStyle('D7:D' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
-        $sheet->getStyle('E7:E' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
-        $sheet->getStyle('G7:G' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
-        $sheet->getStyle('H7:H' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+        // $sheet->getStyle('D7:D' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+        // $sheet->getStyle('E7:E' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+        // $sheet->getStyle('H7:H' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
         $sheet->getStyle('C7:C' . $cell)->getNumberFormat()->setFormatCode('#.##0');
 
-        $sheet->getStyle('A' . $cell . ':I' . $cell)->getFont()->setBold(true);
+        $sheet->getStyle('A' . $cell . ':H' . $cell)->getFont()->setBold(true);
         $sheet->getRowDimension($cell)->setRowHeight(30);
         $sheet->getStyle('A' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
         $border = [
@@ -772,23 +740,11 @@ class dokumenDesaController extends Controller
             ],
         ];
 
-        $sheet->getStyle('A5:I' . $cell)->applyFromArray($border);
+        $sheet->getStyle('A5:H' . $cell)->applyFromArray($border);
         $cell++;
    
 
-        $sheet->setCellValue('E' . ++$cell, '')->mergeCells('E' . $cell . ':I' . $cell);
-            $sheet->getStyle('E' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
-            $sheet->setCellValue('E' . ++$cell, request('nama_jabatan_kepala', ''))->mergeCells('E' . $cell . ':I' . $cell);
-            $sheet->getStyle('E' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
-            $cell = $cell + 3;
-            $sheet->setCellValue('E' . ++$cell, request('nama', ''))->mergeCells('E' . $cell . ':I' . $cell);
-            $sheet->getStyle('E' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
-            $sheet->setCellValue('E' . ++$cell, 'Pangkat/Golongan : ' . request('jabatan', ''))->mergeCells('E' . $cell . ':I' . $cell);
-            $sheet->getStyle('E' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
-            $sheet->setCellValue('E' . ++$cell, 'NIP : ' . request('nip', ''))->mergeCells('E' . $cell . ':I' . $cell);
-            $sheet->getStyle('E' . $cell)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
-
-            $sheet->setCellValue('A' . ++$cell, 'Dicetak melalui ' . url()->current())->mergeCells('A' . $cell . ':L' . $cell);
+            $sheet->setCellValue('A' . ++$cell, 'Dicetak melalui ' . url()->current())->mergeCells('A' . $cell . ':H' . $cell);
             $spreadsheet->getActiveSheet()->getHeaderFooter()
                 ->setOddHeader('&C&H' . url()->current());
             $spreadsheet->getActiveSheet()->getHeaderFooter()
