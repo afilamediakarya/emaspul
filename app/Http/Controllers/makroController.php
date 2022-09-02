a720a2c40749da825a1e43dd5ebb5567860ebbdc
andi akmalul yaqin <94356363+aakmalulyaqin@users.noreply.github.com>
2022-09-02 22:19:45 +0800

    Update makroController.php

--- app/Http/Controllers/makroController.php
+++ app/Http/Controllers/makroController.php
@@ -27,7 +27,7 @@ class makroController extends Controller
 
         $keys = '';
         foreach ($data as $key => $value) {
-            for ($i= 0; $i < 2; $i++) { 
+            for ($i= 0; $i < 3; $i++) { 
                 $keys = 'child'.$i;
                 $value->{$keys} = DB::table('data_makro')->select('target','realisasi')->where('id_indikator_makro',$value->id)->where('tahun',$back_year + $i)->first();
             }
@@ -50,49 +50,86 @@ class makroController extends Controller
 
         $spreadsheet->getProperties()->setCreator('AFILA')
             ->setLastModifiedBy('AFILA')
-            ->setTitle('Alokasi Desa')
-            ->setSubject('Alokasi Desa')
-            ->setDescription('Alokasi Desa')
+            ->setTitle('Capaian Kinerja Makro Enrekang')
+            ->setSubject('Capaian Kinerja Makro Enrekang')
+            ->setDescription('Capaian Kinerja Makro Enrekang')
             ->setKeywords('pdf php')
-            ->setCategory('Alokasi Desa');
+            ->setCategory('Capaian Kinerja Makro Enrekang');
         $sheet = $spreadsheet->getActiveSheet();
         //$sheet->getPageSetup()->setOrientation(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::ORIENTATION_LANDSCAPE);
-        $sheet->getPageSetup()->setPaperSize(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::PAPERSIZE_A4);
-        $spreadsheet->getDefaultStyle()->getFont()->setName('Times New Roman');
+        $sheet->getPageSetup()->setPaperSize(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::PAPERSIZE_FOLIO);
+        $spreadsheet->getDefaultStyle()->getFont()->setName('Bookman Old Style');
         $spreadsheet->getDefaultStyle()->getFont()->setSize(12);
         $spreadsheet->getActiveSheet()->getPageSetup()->setHorizontalCentered(true);
         $spreadsheet->getActiveSheet()->getPageSetup()->setVerticalCentered(false);
-        $sheet->getDefaultRowDimension()->setRowHeight(15);
+        $sheet->getDefaultRowDimension()->setRowHeight(20);
 
         //Margin PDF
         
         $spreadsheet->getActiveSheet()->getPageMargins()->setTop(0.5);
-        $spreadsheet->getActiveSheet()->getPageMargins()->setRight(0.8);
-        $spreadsheet->getActiveSheet()->getPageMargins()->setLeft(1.2);
-        $spreadsheet->getActiveSheet()->getPageMargins()->setBottom(1.0);
+        $spreadsheet->getActiveSheet()->getPageMargins()->setRight(0.5);
+        $spreadsheet->getActiveSheet()->getPageMargins()->setLeft(0.5);
+        $spreadsheet->getActiveSheet()->getPageMargins()->setBottom(0.5);
         $spreadsheet->getActiveSheet()->getStyle('A1:A4')->getAlignment()->setWrapText(true);
 
-        $sheet->setCellValue('A1','No');
-        $sheet->setCellValue('B1','Indikator');
-        $sheet->setCellValue('C1','Target');
-        $sheet->setCellValue('D1','Realisasi');
-        $sheet->setCellValue('E1','Target');
-        $sheet->setCellValue('F1','Realisasi');
+        $sheet->setCellValue('A1', 'CAPAIAN KINERJA MAKRO')->mergeCells('A1:L1');
+        $sheet->setCellValue('A2', ' ')->mergeCells('A2:L2');
+       
+        
+        $sheet->setCellValue('A4','No')->mergeCells('A4:A5')->getColumnDimension('A')->setWidth(5);
+        $sheet->setCellValue('B4','INDIKATOR')->mergeCells('B4:B5')->getColumnDimension('B')->setWidth(40);
+        $sheet->setCellValue('C4','CAPAIAN')->mergeCells('C4:G4');
+        $sheet->setCellValue('H4','TARGET')->mergeCells('H4:L4');
 
-        $cell = 2;
+        $sheet->setCellValue('C5','2019')->getColumnDimension('C')->setWidth(8);
+        $sheet->setCellValue('D5','2020')->getColumnDimension('D')->setWidth(8);
+        $sheet->setCellValue('E5','2021')->getColumnDimension('E')->setWidth(8);
+        $sheet->setCellValue('F5','2022')->getColumnDimension('F')->setWidth(8);
+        $sheet->setCellValue('G5','2023')->getColumnDimension('G')->setWidth(8);
 
+        $sheet->setCellValue('H5','2019')->getColumnDimension('H')->setWidth(8);
+        $sheet->setCellValue('I5','2020')->getColumnDimension('I')->setWidth(8);
+        $sheet->setCellValue('J5','2021')->getColumnDimension('J')->setWidth(8);
+        $sheet->setCellValue('K5','2022')->getColumnDimension('K')->setWidth(8);
+        $sheet->setCellValue('L5','2023')->getColumnDimension('L')->setWidth(8);
+        $sheet->getStyle('A4:L5')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setRGB('C6E0B4');
+        
+        $cell = 6;
         foreach ($data as $key => $value) {
             $sheet->setCellValue('A' . $cell, $key+1);
             $sheet->setCellValue('B' . $cell, $value->indikator_makro);
+
             $sheet->setCellValue('C' . $cell, $value->child0->target);
-            $sheet->setCellValue('D' . $cell, $value->child0->realisasi);
-            $sheet->setCellValue('E' . $cell, $value->child1->target);
-            $sheet->setCellValue('F' . $cell, $value->child1->realisasi);
+            //$sheet->setCellValue('D' . $cell, $value->child1->target);
+            //$sheet->setCellValue('E' . $cell, $value->child2->target);
+            //$sheet->setCellValue('F' . $cell, $value->child3->target);
+            //$sheet->setCellValue('G' . $cell, $value->child4->target);
+
+            $sheet->setCellValue('H' . $cell, $value->child0->realisasi);
+            //$sheet->setCellValue('I' . $cell, $value->child1->realisasi);
+            //$sheet->setCellValue('J' . $cell, $value->child2->realisasi);
+            //$sheet->setCellValue('K' . $cell, $value->child3->realisasi);
+            //$sheet->setCellValue('L' . $cell, $value->child4->realisasi);
+            $cell++;
         }
 
-        $cell++;
+        $border = [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    'color' => ['argb' => '0000000'],
+                ],
+            ],
+        ];
 
-        $sheet->setCellValue('A' . ++$cell, 'Dicetak melalui ' . url()->current())->mergeCells('A' . $cell . ':D' . $cell);
+        $sheet->getStyle('A4:L'. $cell )->applyFromArray($border);
+        $sheet->getStyle('A1:L5')->getFont()->setBold(true);
+        $sheet->getStyle('A1:L'. $cell )->getAlignment()->setVertical('center')->setHorizontal('center');
+        $sheet->getStyle('B6:B'. $cell )->getAlignment()->setVertical('top')->setHorizontal('left');
+        
+
+        $sheet->setCellValue('A' . ++$cell, '');
+        $sheet->setCellValue('A' . ++$cell, 'Dicetak melalui ' . url()->current())->mergeCells('A' . $cell . ':L' . $cell);
             $spreadsheet->getActiveSheet()->getHeaderFooter()
                 ->setOddHeader('&C&H' . url()->current());
             $spreadsheet->getActiveSheet()->getHeaderFooter()
