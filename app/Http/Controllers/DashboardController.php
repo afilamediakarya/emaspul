ce37e8ae3a397b147acb16f0553d4ad4da8397ae
andiabdillahdev <andiabdillahdev@gmail.com>
2022-09-12 18:12:35 +0800

    thun anggran

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,28 +17,24 @@ class DashboardController extends Controller
     public function dashboard_admin(){
         $breadcumb = 'Dashboard';
         $current_breadcumb = '';
-        session(['tahun_penganggaran' => request('tahun', date('Y'))]);
         return view('module.dashboard.index',compact('breadcumb','current_breadcumb'));
     }
 
     public function dashboard_admin_opd(){
         $breadcumb = 'Dashboard';
         $current_breadcumb = '-';
-        session(['tahun_penganggaran' => request('tahun', date('Y'))]);
         return view('module.dashboard.opd',compact('breadcumb','current_breadcumb'));
     }
 
     public function dashboard_admin_desa(){
         $breadcumb = 'Dashboard';
         $current_breadcumb = '-';
-        session(['tahun_penganggaran' => request('tahun', date('Y'))]);
         return view('module.dashboard.desa',compact('breadcumb','current_breadcumb'));
     }
 
     public function dashboard_admin_verifikator(){
         $breadcumb = 'Dashboard';
         $current_breadcumb = '-';
-        session(['tahun_penganggaran' => request('tahun', date('Y'))]);
         return view('module.dashboard.verifikator',compact('breadcumb','current_breadcumb'));
     }
 
