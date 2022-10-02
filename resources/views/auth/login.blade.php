a5cb9542b46bd4c57af66fc61a48cdbfe9a7c0d4
alfiandirham <alfiandirham@gmail.com>
2022-10-03 00:06:58 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
 							</a>
                             <div class="text_page_logo mt-5">BAPPELITBANGDA <br> KABUPATEN ENREKANG</div>
 
-                             <div class="d-flex flex-row-auto bgi-no-repeat bgi-position-x-center bgi-size-contain bgi-position-y-bottom min-h-100px min-h-lg-350px" style="background-image: url(assets/media/illustrations/background_bottom.png)"></div> -->
+                            <!-- <div class="d-flex flex-row-auto bgi-no-repeat bgi-position-x-center bgi-size-contain bgi-position-y-bottom min-h-100px min-h-lg-350px" style="background-image: url(assets/media/illustrations/background_bottom.png)"></div> -->
                             <!--<img class="background_bottom" src="{{ asset('assets/media/illustrations/background_bottom.png') }}" >-->
 
 						</div>
